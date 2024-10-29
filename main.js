72056b85c572dfa27ab6c461e82d5b93c2ad86ae
Davindicator001 <159522633+Davindicator001@users.noreply.github.com>
2024-10-29 03:49:45 +0000

    Update main.js

--- main.js
+++ main.js
@@ -112,13 +112,8 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
          phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
 
          // Ask again when entering the wrong number
-         if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +2348109263390")))
 
-            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 👨🏼‍💻\nFor example: +2348109263390 : `)))
-            phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
             rl.close()
-         }
       }
 
       setTimeout(async () => {
